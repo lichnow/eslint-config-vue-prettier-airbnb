642b71fdf3fb6fc01e5ae53ac6122b01d541d7ed
loveyunk <mjjccc@126.com>
2020-03-04 14:27:57 +0800

    Update

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    es6: true,
+    browser: true
   },
   extends: ['plugin:vue/recommended', '@vue/airbnb', '@vue/prettier'],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
+    ecmaVersion: 2019,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
   },
   rules: {
     'prettier/prettier': 'error'
